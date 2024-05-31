31aef1257784c445c0a2637a271ee732c42db0b9
Simon <vonahkahs.s@gmail.com>
2024-05-31 16:07:25 +0400

    add some media handlers

--- handlers/default_handlers/media_handlers.py
+++ handlers/default_handlers/media_handlers.py
@@ -8,11 +8,43 @@ router = Router()
 any_media_handler = F.photo | F.video | F.document | F.sticker | F.gif | F.survey | F.voice
 
 
-@router.message(any_media_handler, ~F.caption)
-async def media_handler(message: Message):
-	await message.reply(text='I don`t understand you, sorry!. Could you describe it please?')
+@router.message(F.photo, ~F.caption)
+async def photo_handler(message: Message):
+	caption = 'I can`t see, sorry. Could you describe it please?'
+	await message.reply_photo(
+		photo=message.photo[-1].file_id, 
+		caption=caption)
+	
+
+@router.message(F.photo, F.caption)
+async def photo_handler_with_caption(message: Message):
+	caption = f'Cool photo, your text: {message.caption}'
+	await message.reply(text=caption)
 
 
-@router.message(any_media_handler, F.caption)
-async def media_handler_with_caption(message: Message):
-	await message.answer(text='Sorry, i don`t work with this!')
+@router.message(F.video, ~F.caption)
+async def video_handler(message: Message):
+	caption = 'I can`t see, sorry. Could you describe it please?'
+	await message.reply_video(
+		video=message.video.file_id, 
+		caption=caption)
+
+
+@router.message(F.video, F.caption)
+async def video_handler_with_caption(message: Message):
+	caption = f'Cool video, your text: {message.caption}'
+	await message.reply(text=caption)
+
+
+@router.message(F.document, ~F.caption)
+async def document_handler(message: Message):
+	caption = 'I can`t read this doc. Could you describe it please?'
+	await message.reply_document(
+		document=message.document.file_id,
+		caption=caption)
+	
+
+@router.message(F.document, F.caption)
+async def document_handler_with_caption(message: Message):
+	caption = f'Cool document, your text: {message.caption}'
+	await message.reply(text=caption)
