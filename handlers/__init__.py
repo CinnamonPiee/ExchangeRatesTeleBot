1f50b6a7bd0387ab0a59b8398b6e454d89e2220d
Simon <vonahkahs.s@gmail.com>
2024-06-03 14:14:22 +0400

    add some callback handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,8 +4,10 @@ from aiogram import Router
 
 from .default_handlers import router as default_router
 from .custom_handlers import router as custom_router
+from .callback_handlers import router as callback_router
 
 router = Router(name=__name__)
 
+router.include_router(callback_router)
 router.include_router(custom_router)
 router.include_router(default_router)
